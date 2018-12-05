b9cfa9025a7c726a0bcec755b7e0e80eab6578a2
Lea R <lea.rieger@gmx.net>
2018-12-05 17:21:43 +0100

    added data service to item details component

--- src/app/components/item-details/item-details.component.ts
+++ src/app/components/item-details/item-details.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 import {Item} from "../../shared/item-model";
+import {DataService} from "../../services/data.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-item-details',
@@ -9,11 +11,13 @@ import {Item} from "../../shared/item-model";
 })
 export class ItemDetailsComponent implements OnInit {
 
-  private item: Item;
+  private myItem$: Observable<Item>;
 
-  constructor() { }
+  constructor(private dataService: DataService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.myItem$ = this.dataService.getUserItem(this.route.snapshot.paramMap.get('id'));
   }
 
 }

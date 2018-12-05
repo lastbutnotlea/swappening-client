9fc565b0b1bdf36571b16101f1165ec6b21d7a49
Lea R <lea.rieger@gmx.net>
2018-12-05 17:35:21 +0100

    worked on swipe view

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,11 @@
 import {Injectable} from "@angular/core";
 import {ApiService} from "./api.service";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {Item} from "../shared/item-model";
 import {User} from "../shared/user-model";
 import {map} from "rxjs/operators";
+import {merge} from 'rxjs';
+
 
 @Injectable()
 export class DataService {
@@ -11,11 +13,13 @@ export class DataService {
   private user$: Observable<User>;
   private userId: string;
   private myItems$: Observable<Item[]>;
+  private swipeItems$: Observable<Item[]>;
 
   constructor(private apiService: ApiService) {
     this.user$ = this.apiService.getUserDetails();
     this.user$.subscribe(user => this.userId = user.userId);
     this.myItems$ = this.apiService.getAllUserItems(this.userId);
+    this.swipeItems$ = this.apiService.getFirstSwipeItems(this.userId);
   }
 
   public getAllUserItems(): Observable<Item[]> {
@@ -30,5 +34,16 @@ export class DataService {
     return this.user$;
   }
 
+  public getSwipeItems() {
+    return this.swipeItems$;
+  }
+
   // TODO update data; does this work using observables the way i do right now?
+
+  public fetchNewSwipeItems() {
+    let newSwipeItems$: Observable<Item[]>;
+    this.swipeItems$.pipe(map(items => items.slice(10, 15)));
+    newSwipeItems$ = this.apiService.getSwipeItems(this.userId);
+    this.swipeItems$ = merge(this.swipeItems$, newSwipeItems$);
+  }
 }
--- src/app/shared/item-model.ts
+++ src/app/shared/item-model.ts
@@ -74,3 +74,233 @@ export const FAKE_ITEMS: Item[] = [
   }
 ];
 
+export const FAKE_FIRST_SWIPE_ITEMS: Item[] = [
+  {
+    itemId: '1',
+    headline: 'beautiful but useless flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '2',
+    headline: 'a flower!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '3',
+    headline: 'yet another flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '4',
+    headline: 'i am a flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '5',
+    headline: 'two flowers! so wow!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '6',
+    headline: 'beautiful but useless flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '7',
+    headline: 'a flower!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '8',
+    headline: 'yet another flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '9',
+    headline: 'i am a flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '10',
+    headline: 'two flowers! so wow!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '11',
+    headline: 'beautiful but useless flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '12',
+    headline: 'a flower!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '13',
+    headline: 'yet another flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '14',
+    headline: 'i am a flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '15',
+    headline: 'two flowers! so wow!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  }
+];
+
+export const FAKE_SWIPE_ITEMS: Item[] = [
+  {
+    itemId: '1',
+    headline: 'beautiful but useless flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '2',
+    headline: 'a flower!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '3',
+    headline: 'yet another flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '4',
+    headline: 'i am a flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '5',
+    headline: 'two flowers! so wow!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '6',
+    headline: 'beautiful but useless flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '7',
+    headline: 'a flower!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '8',
+    headline: 'yet another flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '9',
+    headline: 'i am a flower',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  },
+  {
+    itemId: '10',
+    headline: 'two flowers! so wow!',
+    description: 'a more detailed description about what this flower can and cannot do',
+    tags: ['flower', 'useless'],
+    ownerId: '001',
+    giveAway: false,
+    imageUrls: []
+  }
+];

e59cfd5b08acb029065f25d0107b47a0e9b4a284
EdoardoMosca <edoardo.mosca@tum.de>
2018-12-10 17:27:55 +0100

    worked on edit item detail view

--- src/app/screens/my-item-details/my-item-details.component.ts
+++ src/app/screens/my-item-details/my-item-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-my-item-details',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyItemDetailsComponent implements OnInit {
 
-  constructor() { }
+  private itemId: string;
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.itemId = this.route.snapshot.paramMap.get('id');
   }
 
 }

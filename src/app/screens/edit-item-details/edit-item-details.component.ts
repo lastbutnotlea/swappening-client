fae256a38ab1990b0dad20b1be9140a3d90109a6
EdoardoMosca <edoardo.mosca@tum.de>
2018-12-10 17:52:52 +0100

    edit view now works for headline and description

--- src/app/screens/edit-item-details/edit-item-details.component.ts
+++ src/app/screens/edit-item-details/edit-item-details.component.ts
@@ -18,9 +18,11 @@ export class EditItemDetailsComponent implements OnInit {
 
   ngOnInit() {
     let current_id: string = this.route.snapshot.paramMap.get('id');
+    console.log(current_id)
     if(current_id != 'new'){
       this.myItem$ = this.dataService.myItem(current_id);
     }
+    console.log(this.myItem$)
   }
 
 }

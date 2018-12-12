8d6701c19899cf169d5da66e7b848f918256a04c
EdoardoMosca <edoardo.mosca@tum.de>
2018-12-12 16:34:29 +0100

    browse and save button added and connected with selected file variable

--- src/app/screens/edit-item-details/edit-item-details.component.ts
+++ src/app/screens/edit-item-details/edit-item-details.component.ts
@@ -3,6 +3,7 @@ import {Observable} from "rxjs";
 import {Item} from "../../shared/item-model";
 import {DataService} from "../../services/data.service";
 import {ActivatedRoute} from "@angular/router";
+import { HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-edit-item-details',
@@ -13,15 +14,28 @@ export class EditItemDetailsComponent implements OnInit {
 
   private myItem$: Observable<Item>;
   myItemModel: Item;
+  selectedFile = null;
 
   constructor(private dataService: DataService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private http: HttpClient) { }
 
   ngOnInit() {
     let current_id: string = this.route.snapshot.paramMap.get('id');
     if(current_id != 'new'){
       this.myItem$ = this.dataService.myItem(current_id);
       this.myItem$.subscribe(newItem => this.myItemModel = newItem);
+    }
+  }
+
+  onFileSelected(event) {
+    console.log(event);
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload() {
+    if(this.selectedFile != null) {
+      console.log("Selected File is not null")
     }
   }
 

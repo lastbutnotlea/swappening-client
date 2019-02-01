8604e299f775e92ef4a7cd45a5c149ea6ce7606c
EdoardoMosca <edoardo.mosca@tum.de>
2019-02-01 15:14:25 +0100

    event details refactored

--- src/app/components/event-details/event-details.component.ts
+++ src/app/components/event-details/event-details.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {Item} from '../../shared/item-model';
-import {Event} from '../../shared/item-model';
+import {Event} from '../../shared/event-model';
 import {DataService} from '../../services/data.service';
 import {ActivatedRoute} from '@angular/router';
 import {environment} from "../../../environments/environment";
@@ -15,6 +15,9 @@ export class EventDetailsComponent implements OnInit {
 
   private itemId: number;
   private myItem$: Observable<Item>;
+  private eventId: number;
+  private event$: Observable<Event>;
+
   private apiUrl: string;
   private clickCounter = 0;
 
@@ -25,7 +28,9 @@ export class EventDetailsComponent implements OnInit {
   ngOnInit() {
     this.apiUrl = environment.apiUrl;
     this.itemId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+    this.eventId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
     this.myItem$ = this.dataService.myItem(this.itemId);
+    this.event$ = this.dataService.event(this.eventId);
   }
 
   cycleThroughPictures() {

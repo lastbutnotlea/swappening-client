4ac225aa622638350bc34bc99d877ca012ab0703
Lea R <lea.rieger@gmx.net>
2019-02-01 11:43:55 +0100

    refactored endpoints

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -59,18 +59,56 @@ export class ApiService {
 
   // TODO: deleteAccount
 
+
   // ### EVENTS HOSTED BY USER
 
   public getHostedEvents(userId: string): Observable<Event[]> {
     if (environment.useMockData) {
       return of(FAKE_EVENTS);
     } else {
-      const requestUrl = environment.apiUrl + `/ofUser/${this.userId}`;
+      const requestUrl = environment.apiUrl + '/ofUser/' + this.userId;
       return this.http.get<Event[]>(requestUrl, {
         headers: {Authorization: 'Bearer ' + this.userToken}
       })
     }
   }
+
+  public createNewHostedEvent(newEvent: Event): Observable<Event> {
+    const requestUrl = environment.apiUrl + '/event';
+    return this.http.post<Event>(requestUrl, {
+      ...newEvent
+    }, {
+      headers: {Authorization: 'Bearer ' + this.userToken}
+    })
+  }
+
+  public uploadPicture(selectedFile: File, eventId: number): Observable<any[]> {
+    const uploadData = new FormData();
+    uploadData.append('data', selectedFile, selectedFile.name);
+    uploadData.append('itemId', eventId.toString());
+    const requestUrl = environment.apiUrl + '/event/image';
+    return this.http.post<any[]>(requestUrl,
+      uploadData,
+      {
+        headers: {Authorization: 'Bearer ' + this.userToken}
+      }
+    );
+  }
+
+  public updateHostedEvent(updatedEvent: Event): Observable<Event> {
+    const requestUrl = environment.apiUrl + '/event/' + updatedEvent.id;
+    return this.http.put<Event>(requestUrl, {
+      ...updatedEvent
+    }, {
+      headers: {Authorization: 'Bearer ' + this.userToken}
+    })
+  }
+
+  // TODO getLikedEvent
+
+  // TODO likeEvent (?)
+
+  // TODO dislikeEvent (?)
 
 
 
@@ -126,10 +164,29 @@ export class ApiService {
   // TODO: deleteUserItem(id: string)
 
 
-
   // ### SWIPE EVENTS
 
-  /*public getFirstSwipeEvents*/
+  public getFirstSwipeEvents(userId: string): Observable<Event[]> {
+    if (environment.useMockData) {
+      return of(FAKE_EVENTS);
+    } else {
+      const requestUrl = environment.apiUrl + '/forUser/' + this.userId + '/' + environment.reloadEvery * 1.5;
+      return this.http.get<Event[]>(requestUrl, {
+        headers: {Authorization: 'Bearer ' + this.userToken}
+      });
+    }
+  }
+
+  public getSwipeEvents(userId: string): Observable<Event[]> {
+    if (environment.useMockData) {
+      return of(FAKE_EVENTS.slice(0,10));
+    } else {
+      const requestUrl = environment.apiUrl + '/forUser/' + this.userId + '/' + environment.reloadEvery;
+      return this.http.get<Event[]>(requestUrl, {
+        headers: {Authorization: 'Bearer ' + this.userToken}
+      })
+    }
+  }
 
   // ### ALL ITEMS
 

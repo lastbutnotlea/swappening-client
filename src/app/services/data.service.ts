ea23c2d714f0e352119e2b130329c2f905f59838
Lea R <lea.rieger@gmx.net>
2019-02-01 13:18:09 +0100

    refactored data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -40,8 +40,15 @@ export class DataService implements OnInit {
         this.apiService.getHostedEvents('1213').subscribe(res => {
           this._hostedEvents.next(res);
           this._hostedEventsLoaded.next(true);
+          // TODO FIX THIS AS SOON AS ALEX PROVIDES THE CORRECT ENDPOINT
+          this._likedEvents.next(res);
+          this._likedEventsLoaded.next(true);
         });
-
+        // this.apiService.getLikesEvents();
+        this.apiService.getFirstSwipeEvents('1213').subscribe(res => {
+          this._swipeEvents.next(res);
+          this._swipeEventsLoaded.next(true);
+        })
       }
     );
   }
@@ -56,6 +63,63 @@ export class DataService implements OnInit {
 
   get hostedEventsLoaded(): Observable<boolean> {
     return new Observable<boolean>(fn => this._hostedEventsLoaded.subscribe(fn));
+  }
+
+  get likedEvents(): Observable<Event[]> {
+    return new Observable<Event[]>(fn => this._likedEvents.subscribe(fn));
+  }
+
+  get likedEventsLoaded(): Observable<boolean> {
+    return new Observable<boolean>(fn => this._likedEventsLoaded.subscribe(fn));
+  }
+
+  get swipeEvents(): Observable<Event[]> {
+    return new Observable<Event[]>(fn => this._swipeEvents.subscribe(fn));
+  }
+
+  get swipeEventsLoaded(): Observable<boolean> {
+    return new Observable<boolean>(fn => this._swipeEventsLoaded.subscribe(fn));
+  }
+
+  event(id: number): Observable<Event> {
+    let findEvent = new Observable<Event[]>(fn =>
+      this._hostedEvents.subscribe(fn)).pipe(map((hostedEvents: Event[]) => hostedEvents.find(event => event.id === id)));
+    if(findEvent) return findEvent;
+    findEvent = new Observable<Event[]>(fn =>
+      this._likedEvents.subscribe(fn)).pipe(map((likedEvents: Event[]) => likedEvents.find(event => event.id === id)));
+    if(findEvent) return findEvent;
+    findEvent = new Observable<Event[]>(fn =>
+      this._swipeEvents.subscribe(fn)).pipe(map((swipeEvents: Event[]) => swipeEvents.find(event => event.id === id)));
+    return findEvent;
+  }
+
+  async createNewHostedEvent(newEvent: Event): Promise<number> {
+    return new Promise<number>((resolve, reject) => {
+      let newEventId;
+      this.apiService.createNewHostedEvent(newEvent).subscribe(res => {
+        newEventId = res.id;
+        this._hostedEvents.next([res].concat(this._hostedEvents.value));
+        resolve(newEventId);
+      });
+    });
+  }
+
+  public uploadPicture(selectedFile: File, eventId: number) {
+    this.apiService.uploadPicture(selectedFile, eventId).subscribe(res => {
+      let editedEventIndex = this._hostedEvents.value.findIndex(event => event.id === eventId);
+      let newEventsArray = this._hostedEvents.value;
+      newEventsArray[editedEventIndex].pictures_events = res;
+      this._hostedEvents.next(newEventsArray);
+    })
+  }
+
+  public updateHostedEvent(newEvent: Event) {
+    this.apiService.updateHostedEvent(newEvent).subscribe(res => {
+      let editedEventIndex = this._hostedEvents.value.findIndex(event => event.id === newEvent.id);
+      let newEventsArray = this._hostedEvents.value;
+      newEventsArray[editedEventIndex] = res;
+      this._hostedEvents.next(newEventsArray);
+    })
   }
 
 
@@ -94,15 +158,6 @@ export class DataService implements OnInit {
         resolve(newItemId);
       });
     });
-  }
-
-  public uploadPicture(selectedFile: File, itemId: number) {
-    this.apiService.uploadPicture(selectedFile, itemId).subscribe(res => {
-      let editedItemIndex = this._myItems.value.findIndex(myItem => myItem.id === itemId);
-      let newItemsArray = this._myItems.value;
-      newItemsArray[editedItemIndex].pictures = res;
-      this._myItems.next(newItemsArray);
-    })
   }
 
   public updateUserItem(newItem: Item) {

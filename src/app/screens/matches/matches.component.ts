d26299f9b237d918bcb9d72a863d6a497b37b22f
EdoardoMosca <edoardo.mosca@tum.de>
2019-03-03 16:10:38 +0100

    displaying event images in chats

--- src/app/screens/matches/matches.component.ts
+++ src/app/screens/matches/matches.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from "@angular/core";
 import {Observable} from "rxjs";
 import {map} from "rxjs/operators";
 import {User} from "../../shared/user-model";
+import {Event} from "../../shared/event-model";
+import {environment} from "../../../environments/environment";
 import {DataService} from "../../services/data.service";
 import {Chat} from "../../shared/chat-model";
 import {ChatService} from "../../services/chat.service";
@@ -17,22 +19,25 @@ export class MatchesComponent implements OnInit {
   private interestedUsersMap$: Observable<Map<number, User[]>>;
   private interestedUsers$: Observable<User[]>; // = this.interestedUsersMap$.pipe(map(userMap => userMap[this.eventId]));
   private chatsOfMyEvents$: Observable<Chat[]>;
-  private chatsOfLikedEvents$: Observable<Chat[]>;
+  private chatsOfLikedEvents$: Observable<Object[]>;
+
+  apiUrl: string;
 
   constructor(private dataService: DataService,
               private chatService: ChatService) {
+    this.apiUrl = environment.apiUrl;
   }
 
   ngOnInit() {
     this.chatsOfMyEvents$ = this.chatService.chatsOfMyEvents;
-    this.chatsOfLikedEvents$ = this.chatService.chatsOfLikedEvents;
+    this.chatsOfLikedEvents$ = this.dataService.getLikedEventsChatsOfLikedEvents()
 
-    this.interestedUsersMap$ = this.dataService.interestedUsers;
+    /*this.interestedUsersMap$ = this.dataService.interestedUsers;
     this.interestedUsersMap$.subscribe(whatever => console.log(whatever));
     this.interestedUsers$ = this.interestedUsersMap$.pipe(map(userMap => {
       return userMap.get(this.eventId);
     }));
-    this.interestedUsers$.subscribe(data => console.log(data));
+    this.interestedUsers$.subscribe(data => console.log(data));*/
   }
 
 }

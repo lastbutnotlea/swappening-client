835300daf516c32655b40c4e219e7fa6bee08b86
Lea R <lea.rieger@gmx.net>
2019-03-04 16:07:36 +0100

    styled the chat! fixed bugs in chat service; implemented auto scroll

--- src/app/screens/chat/chat.component.ts
+++ src/app/screens/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from "@angular/core";
+import {AfterViewChecked, Component, OnInit, ViewChild} from "@angular/core";
 import {ApiService} from "../../services/api.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ChatService} from "../../services/chat.service";
@@ -6,13 +6,16 @@ import {Observable} from "rxjs/Rx";
 import {Chat} from "../../shared/chat-model";
 import {DataService} from "../../services/data.service";
 import {User} from "../../shared/user-model";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: "app-chat",
   templateUrl: "./chat.component.html",
   styleUrls: ["./chat.component.scss"]
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
+  @ViewChild("chatcontainer") chatContainer;
+
   chat$: Observable<Chat>;
   me$: Observable<User>;
   partnerUser$: Observable<User>;
@@ -21,6 +24,8 @@ export class ChatComponent implements OnInit {
   isEventOwner: boolean;
   messageToSend;
   chatSocket;
+  apiUrl: string = '';
+  scrollToBottom = false;
 
   constructor(private apiService: ApiService,
               private route: ActivatedRoute,
@@ -32,13 +37,24 @@ export class ChatComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.apiUrl = environment.apiUrl;
     this.chat$ = this.chatService.chat(this.chatId);
-    this.chat$.subscribe( (chat) => {
+    this.chat$.subscribe((chat) => {
+      if (!chat) return;
       this.isEventOwner = chat.ownerId === +this.dataService.myId;
       this.partnerUserId = this.isEventOwner ? chat.userId : chat.ownerId;
       this.partnerUser$ = this.chatService.partnerUser(this.partnerUserId);
+      this.scrollToBottom = true;
     });
     console.log(", chatId = " + this.chatId + ", isEventOwner = " + this.isEventOwner);
+  }
+
+  ngAfterViewChecked() {
+    if (this.scrollToBottom) {
+      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+      this.scrollToBottom = false;
+    }
+
   }
 
   sendMessage() {
@@ -48,4 +64,6 @@ export class ChatComponent implements OnInit {
     this.messageToSend = "";
   }
 
+  chatDirection1 = isSelf => isSelf ? "chatMsgRight" : "chatMsgLeft";
+  chatDirection2 = isSelf => isSelf ? "bubble-right" : "bubble-left";
 }

e50c87085c914608fdb09dbae35ae3a02e81f20c
EdoardoMosca <edoardo.mosca@tum.de>
2019-03-10 11:01:53 +0100

    event data retrieved also here to implement displayLocation logic

--- src/app/screens/swipe-event-details/swipe-event-details.component.ts
+++ src/app/screens/swipe-event-details/swipe-event-details.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Observable, Subscription} from 'rxjs';
+import {Event} from '../../shared/event-model';
+import {ApiService} from "../../services/api.service";
+import {DataService} from '../../services/data.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-swipe-event-details',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwipeEventDetailsComponent implements OnInit {
 
-  constructor() { }
+  private eventId: number;
+  private event$: Observable<Event>;
+  private displayLocation: boolean;
+
+  private displayLocationSubscription: Subscription;
+  
+  private apiUrl: string;
+
+  constructor(private dataService: DataService,
+              private apiService: ApiService,
+              private router: Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.apiUrl = environment.apiUrl;
+    this.eventId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
+    this.event$ = this.dataService.swipeEvent(this.eventId);
+    this.displayLocationSubscription = this.event$.subscribe(res => {
+      this.displayLocation = !res.isPrivate;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.displayLocationSubscription) this.displayLocationSubscription.unsubscribe();
   }
 
 }
